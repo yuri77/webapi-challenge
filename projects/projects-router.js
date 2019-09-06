f404f76f0e4f74bb826da4a4dc28a762a1ff1d9e
yurika sakae <yurika.sun@gmail.com>
2019-09-06 10:47:15 -0700

    project post endpoint complete

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -7,7 +7,7 @@ router.use(express.json());
 router.get("/", (req, res) => {
   db.get()
     .then(pro => {
-      console.log(pro);
+      //console.log(pro);
       res.status(200).json(pro);
     })
     .catch(err => {
@@ -15,6 +15,25 @@ router.get("/", (req, res) => {
       res
         .status(500)
         .json({ error: "The projects information could not be retrieved." });
+    });
+});
+
+router.post("/", (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    return res.status(400).json({
+      error: "Please provide name and description for the project"
+    });
+  }
+  db.insert({ name, description })
+    .then(pro => {
+      res.status(201).json(pro);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "There was an error saving the post to database" });
     });
 });
 
