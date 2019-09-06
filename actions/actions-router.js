d4d3a5e8ca231df578e4da15236296ce47125a6b
yurika sakae <yurika.sun@gmail.com>
2019-09-06 13:40:24 -0700

    actions get endpoint complete

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -1,7 +1,33 @@
 const router = require("express").Router();
+action = require("../data/helpers/actionModel.js");
+project = require("../data/helpers/projectModel.js");
 
-router.get("/", (req, res) => {
-  res.send("get to /actions");
+router.get("/:id", validateId, (req, res) => {
+  const { id } = req.pro;
+  project
+    .getProjectActions(id)
+    .then(actions => res.status(200).json(actions))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "action data cannot be retrieved" });
+    });
 });
+
+router.post("/:id");
+
+//custom middleware
+
+function validateId(req, res, next) {
+  const { id } = req.params;
+  project.get(id).then(pro => {
+    if (pro) {
+      //console.log("pro from middle", pro);
+      req.pro = pro;
+      next();
+    } else {
+      res.status(404).json({ error: "project with id does not exist" });
+    }
+  });
+}
 
 module.exports = router;
