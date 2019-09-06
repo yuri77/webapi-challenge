b3d418a3e5517070e377fd1fd4042d1746d0264c
yurika sakae <yurika.sun@gmail.com>
2019-09-06 10:27:58 -0700

    project get endpoint created

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../data/helpers/projectModel.js");
 
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.send("get to /projects/");
+  db.get()
+    .then(pro => {
+      console.log(pro);
+      res.status(200).json(pro);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The projects information could not be retrieved." });
+    });
 });
 
 module.exports = router;
